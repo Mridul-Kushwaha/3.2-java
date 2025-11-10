68e0644b1ed866356047523820544c578c056775
Mridul-Kushwaha <23bcs10555@cuchd.in>
2025-11-10 10:17:51 +0530

    Create Student.java

---
+++ PartB/src/main/java/example/b /PartB/src/main/java/example/b/Student.java
@@ -0,0 +1,35 @@
+package example.b;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String email;
+    private Integer age;
+
+    public Student() {}
+
+    public Student(String name, String email, Integer age){
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Integer getId() { return id; }
+
+    public String getName() { return name; }
+    public void setName(String name){ this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email){ this.email = email; }
+
+    public Integer getAge() { return age; }
+    public void setAge(Integer age){ this.age = age; }
+}
