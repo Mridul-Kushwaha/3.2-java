1b9f981d537390c60dfc66a7004a9f0d3146c99f
Mridul-Kushwaha <23bcs10555@cuchd.in>
2025-11-10 10:17:07 +0530

    Create HibernateUtil.java

---
+++ PartB/src/main/java/example/b /HibernateUtil.java
@@ -0,0 +1,25 @@
+package example.b;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory SESSION_FACTORY = build();
+
+    private static SessionFactory build() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return SESSION_FACTORY;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
